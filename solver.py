87bcdd12acd4a7990c30d032ba04078ec3cc9ebc
sallywang147 <sallyjunsongwang@gmail.com>
2024-06-23 11:39:02 +0000

    addunits

--- solver.py
+++ solver.py
@@ -463,8 +463,22 @@ def check_stanford_master_breadth_requirements(course_choices):
             solver.assertFormula(
                 solver.mkTerm(Kind.EQUAL, requirement, solver.mkTrue())
             )
-            
-     # Check satisfiability
+
+    course_units = [
+        #fix with items 
+        cs154, cs157, cs168,  cs254, cs261, cs265, ee364a, ee364b,  phil251,\
+        cs140, cs140e, cs143, cs144, cs149, cs212, cs242, cs243, cs244, cs244b,  cs295, cs316, cs358, ee180, ee282, ee382e,\
+        cs145, cs147, cs148, cs155, cs173, cs221, cs223a, cs224n, cs224u, cs224w, cs227b, cs22b, cs229, cs229m, cs231a, cs231n, cs234, cs236, cs237a, cs245, cs246, cs247, cs248, cs248a, cs251,  cs255, cs273a, cs273b, cs279, cs345, cs347, cs348a, cs348b, cs348c, cs348e, cs348i, cs348k, cs355, cs356, cs373,\
+        cs152, cs181,  cs182, cs256, cs281, cs329t, cs384, amstud133, amstud145, anthro132D, comm118S, comm120w, comm124, comm130d, comm145, comm154, comm166, comm186w, comm230a, comm230b, comm230c, desinst215, desinst240, earthsys213, english184d, engr248, history244f, intlpol268, law4039, me177,  mse193,  mse231,  mse234, mse254, polisci150a, psych215, publpol103f, publpol353b]
+    for unit in course_units:
+        solver.assertFormula(
+            solver.mkTerm(
+                Kind.GEQ,
+                unit,
+                solver.mkInteger(3),
+            )
+        )           
+    # Check satisfiability
     result = solver.checkSat()
     if result.isSat():
         print("The model is satisfiable.")
