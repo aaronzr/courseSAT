6c09fa0b4c1fd2ab3a73b18debd5664d96ab3a56
sallywang147 <sallyjunsongwang@gmail.com>
2024-07-21 04:58:35 +0000

    add demo

--- demo.py
+++ demo.py
@@ -14,11 +14,151 @@ from schema.requirements_formula import (
 from schema.process import process, agent_prompt, RESULTS_DIR
 
 prior_response = []
-requirement_path = "./temp1.txt"
-transcript_path = "./temp2.txt"
+requirement_path = "temp1.txt"
+transcript_path = "temp2.txt"
 TEMP_FILE = "temp_test.py"
 
-def gpt4o_infer(prompt):
+#we need to explicitly tell LLM to fill in none or unknown for Apprval fields.
+#Otherwise, it will fill in false
+def process_individual_transcript(results_dir, transcript_path):
+        transcript = transcript_path
+        name = os.path.basename(transcript_path)
+        transcript_name, _ = name.split(".")
+        prompt = f"""
+        Please fill out a json schema template containing Student (student information from the given transcript),
+        AP_Credits (Advanced Placement title and Earned Units from the given transcript),
+        Courses_Taken (a list of taken courses with relevant course information from the given transcript), 
+        Deviations (a list of taken courses deviated from major or specializaion requirements, but can be approved by an advisor to meet a requirement),
+        Approval (whether an advior has approved a taken course for degree requirements. This is typically false or unknown from the transcript unless
+        otherwise specified), and Cumulative GPA (cumulative GPA for undnergraduate and graduate degrees) 
+        from a given transcript. It's vitally IMPORTANT that you double check and fill in correct information from the given transcript.
+        Here is the transcript: {transcript}. Please output a filled transcript json schema in the following format only. Your output MUST strictly follow the format.
+        ```
+        {{
+        "Student": {{
+                "Name": String,
+                "Program": String, 
+                "StudentID": Integer,
+                "Coterm": Boolean
+        }},
+        "AP_Credits": [
+                {{"Advanced_Placement": String,
+                  "Earned_Units": Integer                   
+                }}
+        ]
+        "Courses_Taken": [
+                {{"Term": String, "Course_ID": String, "Title": String, "Earned_Units": Integer, "Grade": String}},
+                {{"Term": String, "Course_ID": String, "Title": String, "Earned_Units": Integer, "Grade": String}}, 
+                ...
+        ]
+        "Deviations": [
+                {{
+                "Deviated_Course_ID": String or "None" when "Approved"==false
+                "Approved": Boolean,
+                "Approved_By": String or "None" when "Approved"==false,
+        }},
+          {{
+                "Deviated_Course_ID": String or "None" when "Approved"==false
+                "Approved": Boolean,
+                "Approved_By": String or "None" when "Approved"==false,
+        }},
+        ]
+        
+        "Approval": [
+                {{
+                "Approved": Boolean,
+                "Approved_By": String or "None" when "Approved"==false,
+                "Approved_Course_ID": String or "None" when "Approved"==false
+        }},
+          {{
+                "Approved": Boolean,
+                "Approved_By": String or "None" when "Approved"==false,
+                "Approved_Course_ID": String or "None" when "Approved"==false
+        }},
+        ]
+        
+        "Cumulatives": {{
+                "Undergrad_GPA": Real,
+                "Undergrad_Total_Units": Real,
+                "Graduate_GPA": Real,
+                "Graduate_Total_Units": Real,
+        }},
+        }}
+        ```
+        Remember, your json schema output should strictly follow the given format above and your json schema output will be read as a ```file``` directly by json.load(file). 
+        """
+        schema = gpt4_infer(prompt)
+        start = "```json"
+        start2 = "```python"
+        end = "```"
+        if start in schema: 
+                schema_fix = schema.split(start)[1].split(end)[0]
+                if "transcript = " in schema_fix: 
+                        schema_fix = schema_fix.replace("transcript =","").strip()
+        if start2 in schema: 
+                schema_fix = read_code.split(start)[1].split(end)[0]
+                if "transcript = " in schema_fix: 
+                        schema_fix = schema_fix.replace("transcript =","").strip()
+        else:
+                schema_fix = schema
+        if not os.path.exists(results_dir):
+                os.makedirs(results_dir)
+        file = open(f"{results_dir}/{transcript_name}.json", "w+")
+        file.write(schema_fix)  
+        file.close()
+        print(schema_fix)
+        print("=======================================\n") 
+        path = f"{results_dir}/{transcript_name}.json"
+        return path
+
+        
+def automated_code_fixer(path, iterations):
+        for i in range(iterations):
+                cmd = ["python", "schema/schema_test.py", "--t", path]
+                process = subprocess.Popen(cmd, 
+                           stdout=subprocess.PIPE, 
+                           stderr=subprocess.PIPE)
+
+                # wait for the process to terminate
+                out, err = process.communicate()
+                print(f"out:\n {out}")
+                print(f"err:\n {err}")
+                errcode = process.returncode
+                if "Error" in err.decode("utf-8"):
+                        code = open(path, "r")
+                        print("We are going to prompt for transcript json schema fix...\n")
+                        prompt = f"""
+                        Given the error message {err.decode("utf-8")}, please fix the following json file {code.read()} while
+                        preserving the original substance.
+                        """
+                        fixed_code =gpt4_infer(prompt)
+                        print(f"===============error message=======================\n")
+                        print(err.decode("utf-8"))
+                        print(f"==============={i} iteration of fixing code=======================\n")
+                        start = "```python"
+                        start2 = "```json"
+                        end = "```"
+                        if start in fixed_code: 
+                                reformatted_fixed_code = fixed_code.split(start)[1].split(end)[0]
+                        if "transcript =" in fixed_code: 
+                                reformatted_fixed_code = fixed_code.replace("transcript =","")
+                        if start2 in fixed_code: 
+                                reformatted_fixed_code = fixed_code.split(start2)[1].split(end)[0]
+                        else:
+                                reformatted_fixed_code = fixed_code
+                        file = open(f"{path}", "w+")
+                        file.write(reformatted_fixed_code)
+                        file.close()
+                else:
+                        break
+        return path 
+
+def process(transcript_path):
+        path = process_individual_transcript(RESULTS_DIR, transcript_path)
+        val = automated_code_fixer(path, 30)
+        return val
+
+def gpt4_infer(prompt):
 	client = OpenAI(api_key=os.environ.get("OPENAI_API_KEY"))
 	chat_completion = client.chat.completions.create(
 			messages=[
@@ -339,10 +479,7 @@ def run_analysis(transcript_path, requirement_doc):
 	name = os.path.basename(transcript_path)
 	transcript_name, _ = name.split(".")
 	path = f"{RESULTS_DIR}/{transcript_name}.json"
-	if os.path.isfile(path):
-		transcript = path
-	else:      
-		transcript = process(transcript_path)
+	transcript = process(transcript_path)
 	foundations_result, foundations_trace = check_foundations(transcript)
 	if foundations_result.isSat()==False:
 		unsat_results.append("foundations")
@@ -421,7 +558,7 @@ async def main():
 				content="We are going to generate SMT CVC5 formulas for your document. Please specify\
 		 		a specific requirement you'd like to translate.",
 				).send()
-	if res and res.get("value") == "Final Report":
+	if res and res.get("value") == "Final":
 		await cl.Message(
 				content="We are going to generate a final report by running all checks and analysis from the backend...",
 			).send()
@@ -456,7 +593,7 @@ async def run_translator(message: cl.Message):
 		response = await cl.make_async(translate_to_smt)(requirement_path, message.content)
 		await cl.Message(author="ME", content=response).send()
 
-	if prior_response[-1] == "Final Report":
+	if prior_response[-1] == "Final":
 		await cl.Message(
 			content="analyzing the document and the transcript now...",
 		).send()
