93ff51903a6ca702f4ba8b2e99f8a46fadf6df4a
sallywang147 <sallyjunsongwang@gmail.com>
2024-07-21 07:59:17 +0000

    fix

--- demo.py
+++ demo.py
@@ -607,7 +607,12 @@ async def run_translator(message: cl.Message):
 		).send()
 		unsat_results, requirement_dict, unsat_dict = await cl.make_async(run_analysis)(transcript_path, requirement_path)
 		transcript = open(transcript_path, "r")
-		await cl.Message(author="ME", content=f"Here is a list of unsatisfied requirements: {unsat_results}").send()
+		await cl.Message(author="ME", content=f"Here is the translated json schema: {transcript.read()}").send()
+		transcript = open(transcript_path, "r")
+		for i in range(len(unsat_results)): 
+			await cl.Message(author="ME", content=f"Here is a list of unsatisfied requirements: {unsat_results[i]}").send()
+			await cl.Message(author="ME", content=f"Here is a list of unsatisfied requirements: {requirement_dict[i]}").send()
+			await cl.Message(author="ME", content=f"Here is SMT solver core proof for unsatisifed formulas/requirements: {unsat_dict[i]}").send()
 		await cl.Message(author="ME", content=f"Now we are going to generate agent policies for unsatisfied requirements...").send()
 		for i in unsat_results: 
 			if i=="foundations":
